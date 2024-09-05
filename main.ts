dca7fc9b29971094bde65362f97dbced855256c8
Stanislav <8268363@gmail.com>
2024-09-05 21:12:49 +0300

    add deno server

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { typeByExtension } from "https://deno.land/std@0.224.0/media_types/type_by_extension.ts";
+import { extname } from "https://deno.land/std@0.177.0/path/posix.ts";
+
+const staticPath = Deno.cwd() + "/dist/tulip/browser/";
+
+Deno.serve(async (req) => {
+    let path = new URL(req.url).pathname;
+
+    if (path.endsWith("/")) {
+        path += "/index.html"
+    }
+
+    let file;
+
+    try {
+        file = await Deno.open(staticPath + path);
+    } catch(_){
+        try {
+            file = await Deno.open(staticPath + "index.html");
+        } catch (ex) {
+            console.log(ex);
+            if(ex.code === "ENOENT"){
+                return new Response("Not Found", { status: 404 });
+            }
+            return new Response("Internal Server Error", { status: 500 });
+        }
+    }
+    return new Response(file.readable, {
+        headers: {
+            "content-type" : typeByExtension(extname(path))
+        }
+    });
+})
